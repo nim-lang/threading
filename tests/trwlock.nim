6299f29dcec08937cb16d5a61a2330fb45f6a108
planetis-m <b3lievem@yandex.com>
2021-07-26 11:20:09 +0300

    have to disable latches and rwlocks as well

--- tests/trwlock.nim
+++ tests/trwlock.nim
@@ -1,29 +1,30 @@
-import threading/rwlock, std/os
+when declared(broadcast):
+  import threading/rwlock, std/os
 
-const
-  numThreads = 10
-  numIters = 100
+  const
+    numThreads = 10
+    numIters = 100
 
-var
-  rw: RwLock
-  data = 0
-  threads: array[numThreads, Thread[void]]
+  var
+    rw: RwLock
+    data = 0
+    threads: array[numThreads, Thread[void]]
 
-proc routine =
-  for i in 0..<numIters:
-    writeWith rw:
-      let tmp = data
-      data = -1
-      sleep 1
-      data = tmp + 1
+  proc routine =
+    for i in 0..<numIters:
+      writeWith rw:
+        let tmp = data
+        data = -1
+        sleep 1
+        data = tmp + 1
 
-proc frob =
-  init rw
-  for i in 0..<numThreads:
-    createThread(threads[i], routine)
-  for i in 0..<numIters:
-    readWith(rw, assert data >= 0)
-  joinThreads(threads)
-  assert data == numIters * numThreads
+  proc frob =
+    init rw
+    for i in 0..<numThreads:
+      createThread(threads[i], routine)
+    for i in 0..<numIters:
+      readWith(rw, assert data >= 0)
+    joinThreads(threads)
+    assert data == numIters * numThreads
 
-frob()
+  frob()
--- tests/trwlock1.nim
+++ tests/trwlock1.nim
@@ -1,31 +1,32 @@
-import threading/rwlock, std/[random, os]
+when declared(broadcast):
+  import threading/rwlock, std/[random, os]
 
-const
-  numThreads = 10
-  numIters = 100
+  const
+    numThreads = 10
+    numIters = 100
 
-var
-  rw: RwLock
-  data = 0
-  threads: array[numThreads, Thread[void]]
+  var
+    rw: RwLock
+    data = 0
+    threads: array[numThreads, Thread[void]]
 
-proc routine =
-  var r = initRand(getThreadId())
-  for i in 0..<numIters:
-    if r.rand(1.0) <= 1 / numThreads:
-      writeWith rw:
-        let tmp = data
-        data = -1
-        sleep 1
-        data = tmp + 1
-    else:
-      readWith rw:
-        assert data >= 0
+  proc routine =
+    var r = initRand(getThreadId())
+    for i in 0..<numIters:
+      if r.rand(1.0) <= 1 / numThreads:
+        writeWith rw:
+          let tmp = data
+          data = -1
+          sleep 1
+          data = tmp + 1
+      else:
+        readWith rw:
+          assert data >= 0
 
-proc frob =
-  init rw
-  for i in 0..<numThreads:
-    createThread(threads[i], routine)
-  joinThreads(threads)
+  proc frob =
+    init rw
+    for i in 0..<numThreads:
+      createThread(threads[i], routine)
+    joinThreads(threads)
 
-frob()
+  frob()
