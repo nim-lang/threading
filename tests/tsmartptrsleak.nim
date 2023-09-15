ef771f5ce0a356c67ffc38f303e32680dce52e1c
Jaremy Creechley <creechley@gmail.com>
2023-09-15 15:53:52 -0700

    add test for leak

---
+++ tests/tsmartptrsleak.nim
@@ -0,0 +1,51 @@
+import threading/smartptrs
+import std/isolation
+import std/locks
+import threading/atomics
+import threading/channels
+
+var
+  freeCounts: Atomic[int]
+
+type
+  TestObj = object
+
+proc `=destroy`(obj: TestObj) =
+  discard freeCounts.fetchAdd(1, Release)
+
+var
+  thr: array[0..1, Thread[void]]
+  chan = newChan[SharedPtr[TestObj]]()
+
+const
+  # N = 10_000_000
+  # doSleep = false
+  N = 10_000
+  doSleep = true
+
+import std/os
+
+proc threadA() {.thread.} =
+  for i in 1..N:
+    block:
+      var a: SharedPtr[TestObj] = newSharedPtr(unsafeIsolate TestObj())
+      var b = a
+      chan.send(b)
+      assert a.isNil == false # otherwise we don't copy a?
+      when doSleep:
+        os.sleep(1)
+
+proc threadB() {.thread.} =
+  for i in 1..N:
+    block:
+      var b: SharedPtr[TestObj] = chan.recv()
+      when doSleep:
+        os.sleep(1)
+
+createThread(thr[0], threadA)
+createThread(thr[1], threadB)
+joinThreads(thr)
+
+echo "freeCounts: got: ", $int(freeCounts), " expected: ", N
+echo ""
+assert freeCounts.load(Acquire) == N
